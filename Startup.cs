720f835256f2538ad3a05943bd7b080860af4902
Per Orest <per.orest@outlook.com>
2020-06-17 17:14:44 +0200

    Added some stuff in ImageUploader

--- Startup.cs
+++ Startup.cs
@@ -36,7 +36,7 @@ namespace PRORegister
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
-            if (env.IsDevelopment())
+            if (env.IsProduction())
             {
                 app.UseDeveloperExceptionPage();
             }
